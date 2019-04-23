07cf6a2874b9182b11654420f8897b5c7f9f2142
yusufalicezik <yusufalicezik35@gmail.com>
2019-04-24 02:07:41 +0300

    circle Button

--- SegmentControl3/SegmentControl3/ViewController.swift
+++ SegmentControl3/SegmentControl3/ViewController.swift
@@ -8,14 +8,31 @@
 
 import UIKit
 import SwipeableTabBarController
-class ViewController: UIViewController {
+import CircleMenu
+class ViewController: UIViewController,  CircleMenuDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        var tt = TabBarController()
+        let button = CircleMenu(
+            frame: CGRect(x: 200, y: 200, width: 50, height: 50),
+            normalIcon:"bb",
+            selectedIcon:"bb",
+            buttonsCount: 4,
+            duration: 0.4,
+            distance: 120)
+        button.delegate = self
+        button.layer.cornerRadius = button.frame.size.width / 2.0
+        self.view.addSubview(button)
 
     }
+
+    func circleMenu(_ circleMenu: CircleMenu, buttonDidSelected button: UIButton, atIndex: Int){
+
+        print("\(atIndex) . buton seçildi..")
+    }
+    
+
 
 
 }
