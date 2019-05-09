f2586dae518f5ff2c925d52e6253b8c21c6a1806
yusufalicezik <yusufalicezik35@gmail.com>
2019-05-09 22:41:40 +0300

    annotationCst

--- XibCustomPin/XibCustomPin/CustomXib.swift
+++ XibCustomPin/XibCustomPin/CustomXib.swift
@@ -7,21 +7,28 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class CustomXib: UIView {
 
+    
+    @IBOutlet weak var konumLabel: UILabel!
     var onerilecekKonum:String?
     
     func onerConfig(onerilecekKonum:String){
         self.onerilecekKonum = onerilecekKonum
+        konumLabel.text = onerilecekKonum
     }
     
     @IBAction func onerButtonClicked(_ sender: Any) {
         if let onerilecekKonum_ = self.onerilecekKonum {
             print(onerilecekKonum_)
+            
         }
         
-        self.removeFromSuperview() 
+        
+        self.removeFromSuperview()
     }
     
 
