27c4c741b8bdf8cc063994eeca78f6646b287293
yusufalicezik <yusufalicezik35@gmail.com>
2019-05-11 22:23:59 +0300

    ms

---
+++ RNDM/RNDM/Controller/MainVC.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  RNDM
+//
+//  Created by Yusuf ali cezik on 11.05.2019.
+//  Copyright © 2019 Yusuf Ali Cezik. All rights reserved.
+//
+
+import UIKit
+
+enum ThoughtCategory : String {
+    
+    case ciddi = "ciddi"
+    case eglenceli = "eğlenceli"
+    case cilgin = "çılgın"
+    case popular = "popular"
+}
+
+class MainVC: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
