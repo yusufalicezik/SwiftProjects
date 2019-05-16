8ff0d1e5f4eea22f8cbd987ff30178c4603ba986
yusufalicezik <yusufalicezik35@gmail.com>
2019-05-16 15:04:41 +0300

    SlideScroll

---
+++ MSWP/MSWP/View/Slide.swift
@@ -0,0 +1,22 @@
+//
+//  Slide.swift
+//  MSWP
+//
+//  Created by Yusuf ali cezik on 16.05.2019.
+//  Copyright © 2019 Yusuf Ali Cezik. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
